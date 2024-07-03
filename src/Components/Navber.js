6a7493e228ba5635364a2e5e0618309084eb046b
SIFAT HOSSAIN <143956074+sifathossain232@users.noreply.github.com>
2024-07-03 17:25:53 +0600

    Update tick position

--- src/Components/Navber.js
+++ src/Components/Navber.js
@@ -10,23 +10,22 @@ const Navber = () => {
     const [hoverTwo, setHoverTwo] = useState(null)
 
     const [tick, setTick] = useState(false)
-    // console.log(tick)
 
 
     return (
         <div className="bg-blue">
             <div className="w-[1100px] h-[65px] mx-auto flex justify-between items-center">
                 <Image src="/Image/nexzan-logo.webp" alt="Loading..." width={168} height={45} />
-                <ul className="flex gap-5 items-center">
+                <ul className="flex gap-5 items-center   ">
                     {
                         navberData.map((item, index) => (
                             <li
                                 onMouseEnter={() => setHover(index)}
                                 onMouseLeave={() => setHover(null)}
                                 key={index}
-                                className="group relative leading-9"
+                                className="group relative"
                             >
-                                <div className="text-white font-medium flex items-center gap-1 cursor-pointer ">
+                                <div className="text-white font-medium flex items-center  h-[65px] gap-1 cursor-pointer">
                                     <span>{item.title}</span>
                                     {item.menus && (
                                         <span className={`transition-transform duration-200 ${item.menus ? "group-hover:rotate-180" : " "}`}>
@@ -36,8 +35,8 @@ const Navber = () => {
                                 </div>
                                 {hover === index && item.menus && (
                                     <>
-                                    <div className={`size-5 rotate-45 absolute right-5 top-[35px] ${tick ? "bg-blue-500" : "bg-extraDarkBlue"}  z-[1]`}></div>
-                                        <ul className="absolute bg-extraDarkBlue rounded-b-[4px] top-[44px] right-0 z-[9999]">
+                                    <div className={`size-5 rotate-45 absolute right-5 top-[55px] ${tick ? "bg-blue-500" : "bg-extraDarkBlue"}  z-[1]`}></div>
+                                        <ul className="absolute bg-extraDarkBlue rounded-b-[4px] top-[65px] right-0 z-[9999]">
                                             {
                                                 item.menus?.map((menu, i) => (
                                                     <li
@@ -56,7 +55,7 @@ const Navber = () => {
 
                                                         }
                                                         key={i}
-                                                        className="px-5 min-w-full w-[200px] h-[43px] py-1.5 hover:bg-blue-500 relative border-b last:border-b-0 border-blue-500/80"
+                                                        className="px-5 min-w-full w-[200px] h-[43px] py-1.5 hover:bg-blue-500 relative border-b last:border-b-0 border-blue-500/80 transition-transform duration-200"
                                                     >
                                                         <span className=" text-[14px] font-medium text-[#D1D1D1] cursor-pointer">{menu.title}</span>
                                                         {
